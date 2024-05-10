887a4752fd371c4036f17c4c1e93656fb1ed07a7
Holovko Maksym <golovkomaksim852@gmail.com>
2024-05-10 23:24:19 +0300

    mobile

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,30 +1,26 @@
-const animItems = document.querySelectorAll('.anime-items');
+export function animOnScroll() {
+  const animItems = document.querySelectorAll('.anime-items');
 
-if (animItems.length > 0) {
-  window.addEventListener('scroll', animOnScroll);
+  for (let index = 0; index < animItems.length; index++) {
+    const animItem = animItems[index];
+    const animItemHeight = animItem.offsetHeight;
+    const animItemOffset = offset(animItem).top;
+    const animStart = 4;
 
-  function animOnScroll() {
-    for (let index = 0; index < animItems.length; index++) {
-      const animItem = animItems[index];
-      const animItemHeight = animItem.offsetHeight;
-      const animItemOffset = offset(animItem).top;
-      const animStart = 4;
+    let animItemPoint = window.innerHeight - animItemHeight / animStart;
 
-      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+    if (animItemHeight > window.innerHeight) {
+      animItemPoint = window.innerHeight - window.innerHeight / animStart;
+    }
 
-      if (animItemHeight > window.innerHeight) {
-        animItemPoint = window.innerHeight - window.innerHeight / animStart;
-      }
-
-      if (
-        window.scrollY > animItemOffset - animItemPoint &&
-        window.scrollY < animItemOffset + animItemHeight
-      ) {
-        animItem.classList.add('active');
-      } else {
-        if (!animItem.classList.contains('.anim-no-hide')) {
-          animItem.classList.remove('active');
-        }
+    if (
+      window.scrollY > animItemOffset - animItemPoint &&
+      window.scrollY < animItemOffset + animItemHeight
+    ) {
+      animItem.classList.add('active');
+    } else {
+      if (!animItem.classList.contains('.anim-no-hide')) {
+        animItem.classList.remove('active');
       }
     }
   }
@@ -35,7 +31,3 @@ function offset(el) {
   scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   return { top: rect.top + scrollTop };
 }
-
-setTimeout(() => {
-  animOnScroll();
-}, 300);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { Sigil } from './js/welcome';
 import { handleHeaderScroll } from './js/header';
+import { animOnScroll } from './js/animation';
 
 handleHeaderScroll();
 new Sigil();
+
+window.addEventListener('scroll', animOnScroll);
